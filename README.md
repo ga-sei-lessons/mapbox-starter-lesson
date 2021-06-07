14131d3f414fcb0b0aebee8921c82c91358283ef
weston-bailey <wdbailey87@gmail.com>
2021-06-06 22:06:07 -0700

    remove controller

---
+++ README.md
@@ -0,0 +1,28 @@
+# Mapbox lesson
+
+## Links
+
+mapbox on npm:
+
+https://www.npmjs.com/package/mapbox
+
+mapbox API website:
+
+https://www.mapbox.com/
+
+mapbox gl js:
+
+https://docs.mapbox.com/mapbox-gl-js/api/
+
+mapbox keys:
+
+https://account.mapbox.com/access-tokens/
+
+## code snippets
+
+```html
+<script src="https://api.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.js"></script>
+<link href="https://api.mapbox.com/mapbox-gl-js/v1.9.1/mapbox-gl.css" rel="stylesheet" />
+```
+
+###### tags: `lessons`
--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 // required packages
-require('dotenv').config();
-const express = require('express');
-const layouts = require('express-ejs-layouts');
+require('dotenv').config()
+const express = require('express')
+const layouts = require('express-ejs-layouts')
 
 // configure app 
-const app = express();
-const PORT = 3000;
-app.set('view engine', 'ejs');
+const app = express()
+const PORT = 3000
+app.set('view engine', 'ejs')
 
 // middlewares
 app.use(layouts);
-app.use(express.urlencoded({extended: false}));
+// app.use(express.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public'));
 
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding( { accessToken: process.env.MAPBOX_TOKEN });
+
 // controllers
-app.use('/search', require('./controllers/search'));
+// app.use('/search', require('./controllers/search'))
 
 // home route
 app.get('/', (req, res) => {
-  res.render('home');
-});
+  res.render('home')
+})
 
-app.listen(PORT, () => console.log(`mapping on port: ${port}`));
+app.get('/search', (req, res) =>{
+  geocodingClient.forwardGeocode({ 
+    query: `${req.query.city}, ${req.query.state}`
+   })
+   .send()
+   .then(response => {
+     const match = response.body.features[0];
+    console.log(match);
+     res.render('show', { match, mapkey: process.env.MAPBOX_TOKEN })
+   })
+})
+
+app.listen(PORT, () => console.log(`mapping on port: ${PORT} 🗺`))
